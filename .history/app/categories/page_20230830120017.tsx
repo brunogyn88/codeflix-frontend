44b7a5fb1701066c9046189b4dc76f2da91b60dd
Bruno Alves de Sousa <brunoalves@MacBook-Pro-de-Bruno.local>
2023-08-30 12:03:41 -0300

    fix routes and folders

---
+++ .history/app/categories/page_20230830120017.tsx
@@ -0,0 +1,12 @@
+/* Components */
+import { Box, Typography } from "@mui/material";
+
+export default function IndexPage() {
+  return (
+    <Box>
+      <Typography variant="h3" component="h1">
+        CategoryList Page
+      </Typography>
+    </Box>
+  );
+}
